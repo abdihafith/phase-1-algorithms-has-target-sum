ab8941715571b9bb3bb0995b7afcb7a691cded64
Abdihafith <abdyhafith@gmail.com>
2024-04-08 10:00:51 +0300

    complete

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (i = 0; i < array.length; i++) {
+    let difference = target - array[i];
+    for (let j = i + 1; j < array.length; j++)
+      if (array[j] === difference) return true;
+  }
+  return false;
 }
+// console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
 /* 
   Write the Big O time complexity of your function here
+  Time Complexity O(n^2)
+  Space ComplexityO(n)
 */
 
 /* 
   Add your pseudocode here
+  if any two numbers in the array add up to the target,
+  return true
+  else return false
+  e.g [1,2,3,4,5],5======true
+        [1,2,3,4,5]89========false
 */
 
 /*
   Add written explanation of your solution here
 */
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
   console.log("");
-
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
-
 module.exports = hasTargetSum;
